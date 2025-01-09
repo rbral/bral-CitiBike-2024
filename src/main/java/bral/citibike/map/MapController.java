5270cd243553180461afb9d35833e9c1c28b03ec
rbral <rbral@student.touro.edu>
2025-01-09 01:23:18 -0500

    edits

--- src/main/java/bral/citibike/map/MapController.java
+++ src/main/java/bral/citibike/map/MapController.java
@@ -1,14 +1,18 @@
 package bral.citibike.map;
 
 import bral.citibike.CitiBikeService;
+import bral.citibike.CitiBikeServiceFactory;
 import bral.citibike.CitiBikeUtils;
 import bral.citibike.aws.*;
 import bral.citibike.json.*;
+import com.google.gson.Gson;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
 
 import javax.swing.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
@@ -16,11 +20,7 @@ public class MapController
 {
     private MapFrame view;
 
-    //private CitiBikeService cityBikeService;
     private LambdaService lambdaService;
-    private StationObjects stationsResponse;
-    private StatusObjects statusesResponse;
-    private CitiBikeUtils utils;
 
     public MapController(MapFrame view)
     {
@@ -41,6 +41,16 @@ public class MapController
                 new CitiBikeRequestHandler.Coordinates(view.toUserPosition.getLatitude(), view.toUserPosition.getLongitude())
         );
 
+        Gson gson = new Gson();
+        String json = gson.toJson(request);
+
+        // Save it to a file
+        try (FileWriter writer = new FileWriter("stations.json")) {
+            writer.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         lambdaService.callLambda(request)
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.single())
@@ -49,29 +59,6 @@ public class MapController
                     // Handle successful response
                     processLambdaResponse(response);
                 }, error -> error.printStackTrace());
-
-        //cityBikeService = new CitiBikeServiceFactory().getService();
-
-        /*utils = new CitiBikeUtils(stationsResponse, statusesResponse);
-
-        StationObject startStation = utils.closestStationWithBikes(
-                view.fromUserPosition.getLatitude(), view.fromUserPosition.getLongitude());
-        StationObject endStation = utils.closestStationWithSlots(
-                view.toUserPosition.getLatitude(), view.toUserPosition.getLongitude());
-
-        view.startStationPosition = new GeoPosition(startStation.lat, startStation.lon); // Simulated start station
-        view.endStationPosition = new GeoPosition(endStation.lat, endStation.lon);   // Simulated end station
-
-        view.route = List.of(view.fromUserPosition, view.startStationPosition,
-                view.endStationPosition, view.toUserPosition);
-
-        view.waypoints = Set.of(
-                new DefaultWaypoint(view.fromUserPosition),
-                new DefaultWaypoint(view.startStationPosition),
-                new DefaultWaypoint(view.endStationPosition),
-                new DefaultWaypoint(view.toUserPosition)
-        );
-        view.updateMap();*/
     }
 
     private void processLambdaResponse(CitiBikeRequestHandler.CitiBikeResponse response) {
